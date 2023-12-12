2406d4b9e27f3afe85a193173b9594f4625096ee
Anastasia <anastbulokhova@gmal.com>
2023-12-12 11:14:18 +0300

    end

--- Семинары/работа_с_файлами/logger.py
+++ Семинары/работа_с_файлами/logger.py
@@ -7,10 +7,14 @@ def print_contact(file_name): # функция для вывода всех ко
         else:
             print('Список контактов пустой')
 
+
+
 def connect_with_user(): # спрашивает у юзера информацию
     print('Введите имя, фамилию и телефон (например: Иванов Иван 89132456377): ')
     cont_info = input()
     return cont_info
+
+
 
 def add_contact(file_name): # добавить контакт
     with open(file_name, 'r', encoding='utf8') as file:
@@ -19,6 +23,7 @@ def add_contact(file_name): # добавить контакт
     all_cont.append('\n' + new_cont)
     with open(file_name, 'w', encoding='utf8') as file:
         file.writelines(all_cont)
+
 
 
 def find_contact(file_name): # найти контакт
@@ -66,17 +71,22 @@ def delite_contact(file_name): # удаление контакта
 def update_contact(file_name): # изменение контакта
     with open(file_name, 'r', encoding='utf8') as file:
         all_cont = file.readlines()
-    old_cont = input("Введите имя или фамилию контакта: ")
-    new_value = input('Введите измененные данные: ')
+    old_cont = input("Введите имя и фамилию контакта: ")
+
+    print('Чтобы вы хотели изменить? \
+              \n1 - Фамилию \
+              \n2 - Имя \
+              \n3 - Телефон')
+
+    comm = int(input())
+
     for cont in all_cont:
         cont_as_list = cont.strip().split()
-        if old_cont in cont_as_list:
-            old_cont.replace(cont, new_value)
-            all_cont.append(cont)
-        else:
-            print("Контакт не найден")
-    #     cont_as_list = cont.strip().split()
-    #     cont_as_list = list(map(lambda x: x if x in old_cont else new_cont, cont_as_list))
-    # print(f'Обновленный список контактов', cont_as_list)
-    with open(file_name, 'w', encoding='utf8') as file:
-            file.writelines(all_cont)
+        if old_cont in cont_as_list[comm - 1]:
+            new_value = input('Введите измененные данные: ')
+            cont_as_list[comm - 1] = new_value
+            all_cont.remove(cont)
+            all_cont.append('\n' + ' '.join(cont_as_list))
+            print('Контакт изменен')
+            with open(file_name, 'w', encoding='utf8') as file:
+                file.writelines(all_cont)
